7d2e6ccf47aabf5cff49b9f6069c6a5724b403d4
joe mcgee <jrmcgee@asu.edu>
2022-12-21 00:06:11 +0900

    added personal.sql table

---
+++ personal.sql
@@ -0,0 +1,8 @@
+CREATE TABLE personal (
+ first_name VARCHAR(50) NOT NULL,
+ last_name VARCHAR(50) NOT NULL,
+ gender VARCHAR(50) NOT NULL,
+ email VARCHAR(50)
+);
+insert into personal (first_name, last_name,gender,email) values('John', 'Smith', 'Male','johnsmith@yahoo.com');
+insert into personal (first_name, last_name,gender,email) values('Kate', 'Smithy', 'Female','katesmithy@gmail.com');
